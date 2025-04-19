58ec4c1829a47a099e7f72b3bcf64fa871a7df0a
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-04-19 19:29:38 -0300

    <fix> ajust Task Model

--- app/models/task.py
+++ app/models/task.py
@@ -1,17 +1,18 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 from app.database.database import Base
 
-class Tarefa(Base):
+class Task(Base):
     __tablename__ = "task"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    titulo = Column(String(100), nullable=False)
+    nome = Column(String(100), nullable=False)
     descricao = Column(String(255))
-    concluida = Column(Boolean, server_default='0')
     data_criacao = Column(TIMESTAMP(timezone=True), server_default=text('NOW()'))
+    completed = Column(Boolean, server_default='0')
+    user_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
 
     owner = relationship("User", back_populates="tasks")
 
