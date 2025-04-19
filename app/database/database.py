5bdd10e7de51ebfd18946e751120e5880933f7b2
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-04-19 19:29:55 -0300

    <feat> add log to db

--- app/database/database.py
+++ app/database/database.py
@@ -8,7 +8,7 @@ import os
 load_dotenv(override=True)
 database_url = os.getenv("DATABASE_URL")
 
-engine = create_engine(database_url)
+engine = create_engine(database_url, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
