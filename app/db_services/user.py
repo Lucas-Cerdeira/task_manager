602a9b1edc8ac661eddc69709e45ef76ebe22f09
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-04-19 16:30:57 -0300

    <feat> create_user

--- app/db_services/user.py
+++ app/db_services/user.py
@@ -1,9 +1,24 @@
 from pydantic import EmailStr
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.schemas.user import UserCreate
+
 
 class UserDbServices():
 
-    def create_user(user):
-        ...
+    @staticmethod
+    def create_user(db: Session, user: UserCreate):
+        try:
+            user_dict = dict(user)
+            new_user = User(**user_dict)
+            db.add(new_user)
+            db.commit()
+            db.refresh(new_user)
+        except Exception as erro:
+            raise erro
+        else:
+            return new_user
+
 
     def get_user_by_id(user_id: int):
         ...
