5e6bf2f91f8cf8e6ed8cd2554fee11865f74c296
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-04-19 16:31:09 -0300

    <feat> create route create_user

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
-from app.schemas.user import UserBase
+from fastapi import APIRouter, Depends
+from app.schemas.user import UserCreate, UserResponse
+from app.db_services.user import UserDbServices
+from app.database.database import get_db
+from sqlalchemy.orm import Session
+
 
 user_router = APIRouter()
-
 
 
 @user_router.get("/users/", tags=["users"])
 async def read_users():
     return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@user_router.post("/create_user/", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    return UserDbServices.create_user(user=user, db=db)
