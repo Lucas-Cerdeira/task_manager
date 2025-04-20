09c410864af404ee933a9553c260efa127c51ada
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-04-20 10:13:25 -0300

    <feat> create_all

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, status
 from app.routes.user_routes import user_router
 from app.routes.task_router import task_router
+from app.database.database import Base, engine
+from app.models import user, task
+
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
 
 @app.get(
         "/", 
