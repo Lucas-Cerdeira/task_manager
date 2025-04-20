0bc30e22b53efe559d0a11e612bd371818535b66
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-04-20 10:14:03 -0300

    <fix> identation error

--- app/db_services/user.py
+++ app/db_services/user.py
@@ -11,7 +11,6 @@ logger = logging.getLogger(__name__)
 
 class UserDbServices():
 
-    class UserDbServices:
     @staticmethod
     def create_user(db: Session, user_data: UserCreate) -> User:
         """
