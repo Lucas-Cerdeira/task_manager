4bcf8ebcd76ced84e256e6ff742fd33e88d69cc3
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-06-19 22:22:55 +0000

    <feat>

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, status
 from fastapi_mcp import FastApiMCP
 from app.routes.user_routes import user_router
-from app.routes.task_router import task_router
 from app.database.database import Base, engine
 from app.models import user, task
 
@@ -11,7 +10,6 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(user_router)
-app.include_router(task_router)
 
 
 mcp = FastApiMCP(
