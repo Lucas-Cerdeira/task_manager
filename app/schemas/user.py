287e88239625d5559c18a4971e0db3c6b0829b9c
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-08-03 22:40:32 +0000

    <feat> create UserFull

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -13,8 +13,16 @@ class UserResponse(BaseModel):
     nome: str
     sobrenome: str
     email: EmailStr
+    class Config:
+        orm_mode = True
 
 class UserUpdate(BaseModel):
     nome: str = Field(None, min_length=2, max_length=100)
     sobrenome: str = Field(None, min_length=2, max_length=100)
     email: EmailStr = None
+
+class UserFull(UserResponse):
+    id: int
+    tasks: list = []  # Assuming tasks is a list of TaskResponse objects
+    class Config:
+        orm_mode = True
