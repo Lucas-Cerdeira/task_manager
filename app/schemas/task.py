c502cdb540ef133959254958651222ede6be7ca1
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-08-03 22:41:29 +0000

    <feat> create TaskFull

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -5,15 +5,23 @@ from datetime import datetime
 class TaskBase(BaseModel):
     nome: str = Field(..., min_length=2, max_length=100)
     descricao: str = Field(..., min_length=2, max_length=255)
-    completed: bool | None
+    completed: bool = False
+    data_entrega: datetime | None = None
+    diaria: bool = False
 
 
 class TaskCreate(TaskBase):
     ...
 
 class TaskFull(TaskBase):
-    ...
+    id: int
+    user_id: int
+    data_criacao: datetime
+    class Config:
+        orm_mode = True
 
 
 class TaskResponse(TaskBase):
-    data_criacao: datetime
+    ...
+    class Config:
+        orm_mode = True
