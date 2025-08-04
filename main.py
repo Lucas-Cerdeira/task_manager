0915d7d9ce72f8b54d50d80afbb7bd6a3e99113e
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-08-04 01:56:46 +0000

    <feat> include Evento Router

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, status
 from fastapi_mcp import FastApiMCP
 from app.routes.user_routes import user_router
+from app.routes.evento_routes import router as evento_router
 from app.database.database import Base, engine
-from app.models import user, task
+from app.models import user, task, evento
 
 
 app = FastAPI()
@@ -10,6 +11,7 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(user_router)
+app.include_router(evento_router)
 
 
 mcp = FastApiMCP(
