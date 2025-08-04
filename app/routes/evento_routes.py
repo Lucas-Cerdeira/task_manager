c0062d0dca373cab6eea141596d140c893bb7cf8
Lucas Cerdeira <cerdeiralucas4121@gmail.com>
2025-08-04 02:02:38 +0000

    <feat> create event files

---
+++ app/routes/evento_routes.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from typing import List
+from app.database.database import get_db
+from app.db_services.evento import EventoDbServices
+from app.schemas.evento import EventoCreate, EventoFull, EventoUpdate
+
+router = APIRouter(
+    prefix="/users",
+    tags=["eventos"]
+)
+
+@router.post("/{user_id}/create_evento/", status_code=status.HTTP_201_CREATED, response_model=EventoFull)
+def create_evento(user_id: int, evento: EventoCreate, db: Session = Depends(get_db)):
+    """
+    Cria um novo evento para um usuário específico.
+    - user_id: ID do usuário
+    - evento: Dados do evento a ser criado
+    """
+    return EventoDbServices.create_evento(db=db, evento=evento, user_id=user_id)
+
+@router.get("/{user_id}/eventos/", response_model=List[EventoFull])
+def get_eventos_by_user_id(user_id: int, db: Session = Depends(get_db)):
+    """
+    Retorna todos os eventos de um usuário específico.
+    - user_id: ID do usuário
+    """
+    return EventoDbServices.get_eventos_by_user_id(db=db, user_id=user_id)
+
+@router.put("/{user_id}/eventos/{evento_id}/", response_model=EventoFull)
+def update_evento(user_id: int, evento_id: int, evento_update: EventoUpdate, db: Session = Depends(get_db)):
+    """
+    Atualiza um evento específico.
+    - user_id: ID do usuário
+    - evento_id: ID do evento a ser atualizado
+    - evento_update: Dados a serem atualizados
+    """
+    return EventoDbServices.update_evento(db=db, evento_id=evento_id, evento_update=evento_update)
+
+@router.delete("/{user_id}/eventos/{evento_id}/", status_code=status.HTTP_200_OK, response_model=EventoFull)
+def delete_evento(user_id: int, evento_id: int, db: Session = Depends(get_db)):
+    """
+    Remove um evento específico.
+    - user_id: ID do usuário
+    - evento_id: ID do evento a ser removido
+    """
+    return EventoDbServices.delete_evento(db=db, evento_id=evento_id, user_id=user_id)
---
+++ app/schemas/evento.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+class EventoBase(BaseModel):
+    titulo: str = Field(..., min_length=2, max_length=100)
+    descricao: str = Field(..., min_length=2, max_length=255)
+    local: str = Field(..., min_length=2, max_length=255)
+    data_evento: datetime
+
+class EventoCreate(EventoBase):
+    pass
+
+class EventoFull(EventoBase):
+    id: int
+    user_id: int
+    
+    class Config:
+        orm_mode = True
+
+class EventoUpdate(BaseModel):
+    titulo: str | None = Field(None, min_length=2, max_length=100)
+    descricao: str | None = Field(None, min_length=2, max_length=255)
+    local: str | None = Field(None, min_length=2, max_length=255)
+    data_evento: datetime | None = None
